722778621f69332b7b2ce46fe313855b2a47b078
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-03-19 12:28:40 +0100

    Létrehozzuk a name validátort

--- Validation/NameValidator.cs
+++ Validation/NameValidator.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using Kreta.Desktop.Validation.ValidationRules;
+using System.Globalization;
 using System.Windows.Controls;
 
 namespace Kreta.Desktop.Validation
@@ -7,7 +8,11 @@ namespace Kreta.Desktop.Validation
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-
+            if (value is string)
+            {
+                string nameToValidate = (string)value;
+                NameValidationRules nvr = new NameValidationRules(nameToValidate);
+            }
             return new ValidationResult(true, "");
         }
     }
